84e729037e971107ff55e80e325c2d9b4b0b1b06
Patrik Poklop <151776170+imang212@users.noreply.github.com>
2026-01-05 10:28:03 +0100

    Promazani zbytecnych souborů

--- AI_traffic_detection(hailo)/model_compile/model_export.py
+++
@@ -1,3 +0,0 @@
-from ultralytics import YOLO
-model = YOLO("yolov8m.pt")
-model.export(format="onnx", opset=11, simplify=True)
--- RaspberryPi/HardwareVersion/HailoTrainer.py
+++
@@ -1,6 +0,0 @@
-from yolov5.models.yolo import Model
-import torch
-from ultralytics import YOLO
-
-model = YOLO("yolov5s.pt")  # supports older YOLOv5 weights too
-model.export(format="onnx")  # directly export to ONNX
